d15bd77a81235e73bd08d546d5faddcf612d5dee
ADITYA964 <adityab110699@gmail.com>
2022-05-23 01:20:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,50 @@
+# ====================== Packages ======================
+
+# Libraries to perform file transfer operations, 
+# calculating time of execution, find path of file.
+
 import os
 import sys
-import warnings
-import matplotlib.pyplot as plt
-import cv2
-
 import glob
+import time
 import shutil 
+import warnings
 
+# Libraries that are used to read an image, preprocess 
+# input data, plot figures and convert I/O byte into 
+# equivalent file format.
+
+import cv2
+import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 from IPython.display import display
-import streamlit as st
 from tempfile import NamedTemporaryFile
 
+# Library to connect backend operations with 
+# frontend webpage of application.
+import streamlit as st
+
+# Libraries used to create deep learning models and 
+# preprocessing dataset.
 
 import tensorflow as tf
-from keras.preprocessing import image as image_utils
-from keras.applications.imagenet_utils import decode_predictions
-from keras.applications.imagenet_utils import preprocess_input
 from tensorflow.keras.applications import VGG16
-import numpy as np
-import cv2
+from keras.preprocessing import image as image_utils
+from keras.applications.imagenet_utils import preprocess_input
+from keras.applications.imagenet_utils import decode_predictions
 
-import time
-
+# Give title to the webpage.
 st.write("""
          # Image Classification application 
          """
          )
-
+# Read input image in PNG or JPG format and 
+# store into I/O byte format for further operations.
 file = st.file_uploader("Please upload image to be classified", type=["png","jpg"])
 temp_file = NamedTemporaryFile(delete=False)         
 
-
+# Check whether the user provides the file.
 if file is None:
   st.markdown("<div><span class='highlight green'><font color='white'>Please upload an image file in PNG or JPG format</font></span><div>", unsafe_allow_html=True)  
 else:
